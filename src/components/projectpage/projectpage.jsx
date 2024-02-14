bad8dcba51ebef5c7402a835357be55128cc0c2f
Oyinkansola <tellaoyinkansola25@gmail.com>
2024-02-14 16:28:10 +0100

    updates

--- src/components/projectpage/projectpage.jsx
+++ src/components/projectpage/projectpage.jsx
@@ -34,7 +34,7 @@ const ProjectPage = () => {
         [
             {
                 id: 1,
-                title: "Pledge with no reward",
+                name: "Pledge with no reward",
                 details:"Choose to support without a reward if you simply believe in our project.As a backer,you will be signed up to recieve product updates via email.",
             },
     
@@ -104,7 +104,6 @@ const ProjectPage = () => {
 
     const handleButtonClick = (index) => {  
       
-        const number = parseFloat(formField);   
         const {formFieldValue} = formField[index];
         if(index === 0 && formFieldValue > 0){
             setIsLoading(true);
@@ -258,6 +257,7 @@ const ProjectPage = () => {
                                                             <div className='pledge'>Enter your pledge</div>
     
                                                         <div className='input'>
+
                                                             <div className='input-page'>
                                                                 <input className={`user-input ${formError ? "red" : "black"}`}
                                                                 type='text'
@@ -269,7 +269,7 @@ const ProjectPage = () => {
                             
                                                             <div className='input-button-container'>
                                                                 <div className='input-button'>
-                                                                    <button onClick={() =>handleButtonClick(index)}>Continue</button>
+                                                                    <button type='button' onClick={() =>handleButtonClick(index)}>Continue</button>
                                                                 </div>
 
                                                                 <div>
@@ -279,13 +279,6 @@ const ProjectPage = () => {
                                                                     : ""}
                                                                 </div>
 
-
-                                                        
-                                                                        
-                                                                        
-                                                                        
-                                                                        
-                                                                
                                                                                                                                  
                                                             </div>
                                                         </div>
@@ -305,288 +298,7 @@ const ProjectPage = () => {
             }
         
         </div>
-//     <div>
-//     {
-//         currentReward && (
-//             <div className= "pop-up-container">
-//     <div className='popup-body'>    
-//     <div className='pop-up'>
-//         <div className='project-heading-container'>
-//             <div className='project-heading'>
-//                 <div>Back this project</div>
-//                 <div onClick={handleClose}>
-//                     <svg width="14" height="15" xmlns="http://www.w3.org/2000/svg"><g fill="blackr" fill-rule="evenodd"><path d="M2.404.782l11.314 11.314-2.122 2.122L.282 2.904z"/><path d="M.282 12.096L11.596.782l2.122 2.122L2.404 14.218z"/></g></svg>
-//                 </div>
-//             </div>
-//             <div className='project-heading-text'>Want to support us in bringing Mastercraft Bamboo Monitor Riser out in the world?</div>
-//         </div>
 
-//         <div className='project-box'>
-//             <div className='project-container'>
-//             <div className='project-check-container'>
-//                 <div className='project-check-one'>
-//                     <div className='radio-btn'>
-//                         <input 
-//                         type='radio' 
-//                         id="radio1"
-//                         name="check"
-//                         value="radio1"
-//                         checked={isChecked === "radio1"} 
-//                         onChange={() => toggleCheck('radio1')}></input>
-//                     </div>
-                    
-//                     <span>Pledge with no reward  </span>
-//                 </div>
-
-//                 <div>
-//                     Choose to support without a reward if you simply believe in our project.As a 
-//                     backer,you will be signed up to recieve product updates via email.
-//                 </div>
-
-//                 <div>
-//                 {isChecked === "radio1" && (
-//                     <div className='project-span-input'>
-//                         <div>Enter your pledge </div>
-
-//                         <div className='input'>
-//                             <div className='input-page'>
-//                                 <input className="user-input"
-//                                 // id='field1'
-//                                 id="field1"
-//                                 type='text' 
-//                                 onChange={handleChange}
-//                                 value={textForm.field1}
-//                                 name="field1">
-//                                 </input>
-//                             </div>
-                            
-                            
-//                             <div className='input-button'>
-//                                 <button type='button' onClick={() => handleButtonClick('field1')}>Price</button>
-//                             </div>
-                            
-//                         </div>
-//                     </div>
-//                     )
-//                 }   
-//             </div>
-//             </div>
-  
-//             </div>
-
-//             <div className='project-container'>
-//             <div className='project-check-box'>
-
-//                 <div>
-//                 <div className='project-check-container'>
-//                     <div className='project-check'>
-//                         <div className='check'>
-
-//                             <div className='radio-btn'>
-//                                 <input type='radio'
-//                                 id='radio2'
-//                                 name='check'
-//                                 value='radio2'  
-//                                 checked={isChecked === "radio2"} 
-//                                 onChange={() => toggleCheck('radio2')}></input>
-//                                 <div>Bamboo Stand</div>
-//                             </div> 
-                
-//                             <div>Plede 25 or more</div>
-//                         </div>
-
-//                         <div className='project-count'>{count} <span>left</span></div>
-//                     </div> 
-                    
-//                     <div>You get an ergonomic stand made of natural bamboo. 
-//                     You've helped us launch our promotional campaign,
-//                     and you'll be added to a special Backer member list.
-//                     </div>
-//                 </div>
-
-
-//                 <div>
-//                     {isChecked === "radio2" && (
-//                         <div className='project-span-input'>
-//                             <div>Enter your pledge</div>
-
-//                             <div className='input'>
-//                                 <div className='input-page'>
-//                                     <input className={`user-input ${formError ? "red" : "white"}`}
-//                                     type='text' 
-//                                     onChange={handleChange}
-//                                     value={textForm.field2}
-//                                     name="field2"
-//                                     id='field2'
-//                                     ></input>
-
-//                                 </div>
-                                
-                                
-//                                 <div className='input-button'>
-//                                     <button type='button' onClick={handleButtonClick}>Price</button>
-//                                 </div>
-                                
-//                             </div>
-//                         </div>
-                        
-//                         )
-//                     }
-                    
-//                 </div>
-//                 </div>
-//             </div>
-
-    
-//         </div>
-        
-
-//         <div className='project-container'>
-//         <div className='project-check-box'>
-
-//             <div>
-//             <div className='project-check-container'>
-//                 <div className='project-check'>
-//                     <div className='check'>
-
-//                         <div className='radio-btn'>
-//                             <input 
-//                             type='radio'  
-//                             name='check'
-//                             value='radio3'
-//                             id='radio3'
-//                             checked={isChecked === "radio3" } 
-//                             onChange={() => toggleCheck("radio3")}></input>
-//                             <div>Black Edition Stand</div>
-//                         </div> 
-            
-//                         <div>Plede $75 or more</div>
-//                     </div>
-
-//                     <div className='project-count'>{count} <span>left</span></div>
-//                 </div> 
-                
-//                 <div>You get an ergonomic stand made of natural bamboo. 
-//                 You've helped us launch our promotional campaign,
-//                 and you'll be added to a special Backer member list.
-//                 </div>
-//             </div>
-
-
-//             <div>
-//                 {isChecked === "radio3" && (
-//                     <div className='project-span-input'>
-//                         <div>Enter your pledge</div>
-
-//                         <div className='input'>
-//                             <div className='input-page'>
-//                                 <input className={`user-input ${formError ? "red" : "white"}`}
-//                                 type='text' 
-//                                 onChange={handleChange}
-//                                 value={textForm.field3}
-//                                 name="field3"
-//                                 id='field3'
-//                                 ></input>
-
-//                             </div>
-                            
-                            
-//                             <div className='input-button'>
-//                                 <button type='button' onClick={handleButtonClick}>Price</button>
-//                             </div>
-                            
-//                         </div>
-//                     </div>
-                    
-//                     )
-//                 }
-                
-//             </div>
-//             </div>
-//         </div>
-
-
-//     </div>
-    
-
-//     <div className='project-container'>
-//     <div className='project-check-box'>
-
- 
-//         <div className='project-check-container'>
-//             <div className='project-check'>
-//                 <div className='check'>
-
-//                     <div className='radio-btn'>
-//                         <input 
-//                         type='radio'
-//                         value='radio4'
-//                         name='check'
-//                         id='radio4'  
-//                         checked={isChecked === "radio4"} 
-//                         onChange={() => toggleCheck("radio4")}></input>
-//                         <div>Mahogany Special Editon</div>
-//                     </div> 
-        
-//                     <div>Plede $200 or more</div>
-//                 </div>
-
-//                 <div className='project-count'>{count} <span>left</span></div>
-//             </div> 
-            
-//             <div>You get two Special Edition Mahogany stands, a Backer T-Shirt, and a personal thank you. You'll be 
-//               added to Our Backer member list. Shipping is included.
-//             </div>
-//         </div>
-
-
-//         <div>
-//             {isChecked === "radio4" && (
-//                 <div className='project-span-input'>
-//                     <div>Enter your pledge</div>
-
-//                     <div className='input'>
-//                         <div className='input-page'>
-//                             <input className={`user-input ${formError ? "red" : "white"}`}
-//                             type='text' 
-//                             onChange={handleChange}
-//                             value={textForm.field4}
-//                             name="field4"
-//                             id='field4'
-//                             ></input>
-
-//                         </div>
-                        
-                        
-//                         <div className='input-button'>
-//                             <button type='button' onClick={handleButtonClick}>Price</button>
-//                         </div>
-                        
-//                     </div>
-//                 </div>
-                
-//                 )
-//             }
-            
-//         </div>
-//         </div>
-//     </div>
-
-
-// </div>
-
-
-
-
-
-         
-//         </div>
-//         </div>
-//         </div>
-//         )
-//     }
-    
-//     </div>
     )
 }
 
